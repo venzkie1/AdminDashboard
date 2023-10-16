d7d8c7e1a755926aa876b09804ff425b527048c9
kevin <kevinmembreve32@gmail.com>
2023-10-16 09:47:40 +0800

    first commit

---
+++ database/migrations/2023_10_15_225018_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('price');
+            $table->string('product_code');
+            $table->string('description');
+            
+            // Add the user_id column with a default value of 0
+            $table->unsignedBigInteger('user_id')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
